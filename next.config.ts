cfa8bba04a5609181f4e0a3a8300a127bc11728a
Your Name <you@example.com>
2025-08-25 14:21:01 +0530

    r2 path added

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,15 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pub-6824d00f4db04fc4a2c7d964d8a1f9c9.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "40mb",
